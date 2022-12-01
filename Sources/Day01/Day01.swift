ef326f733df8dd1bfedbed469ce567d6b5d4d5cc
Pascal Batty <pascal@zen.ly>
2022-12-01 16:00:43 +0100

    Solution to day 1

--- Sources/Day01/Day01.swift
+++ Sources/Day01/Day01.swift
@@ -15,10 +15,9 @@ import Common
 
 @main
 struct Day01: Puzzle {
-    // TODO: Start by defining your input/output types :)
     typealias Input = String
-    typealias OutputPartOne = Never
-    typealias OutputPartTwo = Never
+    typealias OutputPartOne = Int
+    typealias OutputPartTwo = Int
 }
 
 // MARK: - PART 1
@@ -26,13 +25,49 @@ struct Day01: Puzzle {
 extension Day01 {
     static var partOneExpectations: [any Expectation<Input, OutputPartOne>] {
         [
-            // TODO: add expectations for part 1
+            assert(expectation: 2000, from: "2000"),
+            assert(expectation: 3000, from:
+"""
+2000
+1000
+"""
+                  ),
+            assert(expectation: 3000, from:
+"""
+2000
+1000
+
+1000
+"""
+                   ),
+            assert(expectation: 5000, from:
+"""
+1000
+2000
+
+2000
+1000
+2000
+"""
+                   ),
         ]
     }
 
     static func solvePartOne(_ input: Input) async throws -> OutputPartOne {
-        // TODO: Solve part 1 :)
-        throw ExecutionError.notSolved
+        (input + ["\n"])
+            .components(separatedBy: .newlines)
+            .reduce(into: (best: 0, current: 0)) { acc, new in
+                if let intValue = Int(new) {
+                    acc.current += intValue
+                    print(acc.current)
+                } else {
+                    if acc.current > acc.best {
+                        acc.best = acc.current
+                    }
+                    acc.current = 0
+                }
+            }
+            .best
     }
 }
 
@@ -41,12 +76,58 @@ extension Day01 {
 extension Day01 {
     static var partTwoExpectations: [any Expectation<Input, OutputPartTwo>] {
         [
-            // TODO: add expectations for part 2
+            assert(expectation: 11000, from:
+"""
+1000
+2000
+
+2000
+1000
+2000
+
+3000
+"""
+                   ),
+            assert(expectation: 11000, from:
+"""
+1000
+2000
+
+1000
+
+2000
+1000
+2000
+
+3000
+"""
+                   ),
         ]
     }
 
     static func solvePartTwo(_ input: Input) async throws -> OutputPartTwo {
-        // TODO: Solve part 2 :)
-        throw ExecutionError.notSolved
+        (input + ["\n"])
+            .components(separatedBy: .newlines)
+            .reduce(into: (tops: [], current: Int(0))) { acc, new in
+                if let intValue = Int(new) {
+                    acc.current += intValue
+                    print(acc.current)
+                } else {
+                    tryInsert(acc.current, in: &acc.tops)
+                    acc.current = 0
+                }
+            }
+            .tops
+            .reduce(0, +)
+    }
+
+    static func tryInsert(_ candidate: Int, in tops: inout [Int]) {
+        let maxTops = 3
+
+        guard tops.count < maxTops || candidate > tops[0] else { return }
+
+        tops.append(candidate)
+        tops.sort()
+        tops = Array(tops.suffix(maxTops))
     }
 }
